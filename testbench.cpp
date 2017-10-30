e4f47b155d7e98e854f6ab2ce15503efa2c099f8
jf5979 <jean-francois.penven.1@etsmtl.net>
2017-10-30 09:41:08 -0400

    Change rw function in testbench

--- testbench.cpp
+++ testbench.cpp
@@ -12,12 +12,11 @@
 #include "buf.h"
 
 int main(void){
-    std::cout<<"Ecriture dans le driver\n";
-    char saisie_de_user[16];
-    char lecture_fichier[16];
+    char saisie_de_user[256];
+    char lecture_fichier[256];
     char input=0x00,lecture=0;
-    FILE *fp;
-    int file_descriptor;
+    int i=0,nb_char=0;
+    int fp;
 
 
 
@@ -28,60 +27,62 @@ int main(void){
         std::cin>>input;
         switch (input) {
             case '1' :
-                std::cout<<"Veuillez entrer une chaine de moins de 15 characteres suivit de [ENTER]";
+                std::cout<<"Veuillez entrer une chaine de characteres suivit de [ENTER]";
                 std::cin>>saisie_de_user;
-                fp=fopen("/dev/Buf_node","w+");
-                if (fp == NULL) {
+                fp=open("/dev/Buf_node",O_WRONLY);
+                if (fp < 0) {
                     printf("Error couldnt open the file\n");
                 } else {
                     std::cout << "Fichier ouvert avec success en ecriture\n";
-                    fprintf(fp, "%s",saisie_de_user);
+                    i=0;
+                    while(saisie_de_user[i]!='\0'){
+                        i++;
+                    }
+                    std::cout<<"Number of character is : "<<i<<'\n';
+                    write(fp,(char*) saisie_de_user,i);
+
                     std::cout << "Ecriture completer, fermeture du fichier\n";
-                    fclose(fp);
+                    close(fp);
                     std::cout << "Fichier fermer\n";
                 }
                 break;
             case '2':
-                fp=fopen("/dev/Buf_node","r");
-                if (fp == NULL) {
+                fp=open("/dev/Buf_node",O_RDONLY);
+                if (fp < 0) {
                     printf("Error couldnt open the file\n");
                 } else {
                     std::cout << "Fichier ouvert avec succes en lecture\n";
-                    for(int i=0;i<16;i++){
-                        fscanf(fp,"%c",&(lecture_fichier[i]));
-                        std::cout<<lecture_fichier[i];
-                        if(lecture_fichier[i]=='\0'){
-                            break;
-                        }
-                    }
-                    std::cout << "\nLecture completer, fermeture du fichier\n";
-                    fclose(fp);
+                    std::cout<<"Entrez le nombre de charactere a lire\n";
+                    std::cin>>nb_char;
+                    read(fp,(char *) lecture_fichier,nb_char);
+                    std::cout << lecture_fichier;
+                    close(fp);
                     std::cout << "Fichier fermer\n";
-                    for(int i=0;i<16;i++){
+                    for(int i=0;i<256;i++){
                         lecture_fichier[i]='\0';
                     }
                 }
                 break;
             case '3':
-                fp=fopen("/dev/Buf_node","r");
-                if (fp == NULL) {
+                fp=open("/dev/Buf_node",O_RDONLY);
+                if (fp < 0) {
                     printf("Error couldnt open the file\n");
                 } else {
-                    fclose(fp);
+                    close(fp);
                 }
                 break;
             case '4':
 
-                file_descriptor=open("/dev/Buf_node",O_RDWR);
-                if (file_descriptor<0 ) {
+                fp=open("/dev/Buf_node",O_RDWR);
+                if (fp < 0 ) {
                     printf("Error couldnt open the file\n");
                 } else {
-                    std::cout<<"Nombre de donnee    : "<<ioctl(file_descriptor,BUFF_GETNUMDATA)<<"\n";
+                    std::cout<<"Nombre de donnee    : "<<ioctl(fp,BUFF_GETNUMDATA)<<"\n";
 //                    std::cout<<"Nombre de lecteur   : "<<ioctl(file_descriptor,1)<<"\n";
 //                    std::cout<<"Taille du Buffer    : "<<ioctl(file_descriptor,2)<<"\n";
                     std::cout<<"Voulez-vous modifier la taille du buffer (Y/n)";
 
-                    close(file_descriptor);
+                    close(fp);
                 }
 
                 break;
