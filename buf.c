d9e8a517e52123e46ad139a6f45d023ee9b03da8
jf5979 <jean-francois.penven.1@etsmtl.net>
2017-11-05 14:35:08 -0500

    Implementation de wait_queue pour rw

--- buf.c
+++ buf.c
@@ -12,6 +12,7 @@
 #include <linux/device.h>
 #include <asm/atomic.h>
 #include <asm/uaccess.h>
+#include <linux/sched.h>
 
 #include "buf.h"
 
@@ -74,6 +75,9 @@ struct file_operations Buf_fops = {
 
 BDev Buffer_Tool;
 Buffer Le_buffer;
+static DECLARE_WAIT_QUEUE_HEAD(reading_queue);
+static DECLARE_WAIT_QUEUE_HEAD(writing_queue);
+
 
 int BufIn (struct BufStruct *Buf, unsigned short *Data) {
     if (Buf->BufFull) {
@@ -229,13 +233,13 @@ int Buf_release(struct inode *inode, struct file *filp) {
                 Buffer_Tool.numReader--;
             }
             up(&(Buffer_Tool.SemBuf));
-            printk(KERN_ALERT "Buf was open in Read : releasing driver (%s:%u)\n", __FUNCTION__, __LINE__);
+            printk(KERN_WARNING "Buf was open in Read : releasing driver (%s:%u)\n", __FUNCTION__, __LINE__);
             break;
         case O_WRONLY :
             down_interruptible(&(Buffer_Tool.SemBuf));
             if(Buffer_Tool.numWriter>0){
                 Buffer_Tool.numWriter--;
-                printk(KERN_ALERT "Buf was open in Write Only : releasing writer lock (%d writer) (%s:%u)\n",Buffer_Tool.numWriter, __FUNCTION__, __LINE__);
+                printk(KERN_WARNING "Buf was open in Write Only : releasing writer lock (%d writer) (%s:%u)\n",Buffer_Tool.numWriter, __FUNCTION__, __LINE__);
             }
             up(&(Buffer_Tool.SemBuf));
             break;
@@ -248,7 +252,7 @@ int Buf_release(struct inode *inode, struct file *filp) {
                 Buffer_Tool.numReader--;
             }
             up(&(Buffer_Tool.SemBuf));
-            printk(KERN_ALERT "Buf was open in Read Write : releasing writer lock (%d writer) (%s:%u)\n",Buffer_Tool.numWriter, __FUNCTION__, __LINE__);
+            printk(KERN_WARNING "Buf was open in Read Write : releasing writer lock (%d writer) (%s:%u)\n",Buffer_Tool.numWriter, __FUNCTION__, __LINE__);
             break;
         default:
             break;
@@ -262,94 +266,68 @@ static ssize_t Buf_read(struct file *flip, char __user *ubuf, size_t count, loff
         int i=0, j=0,nb=0,k=0,l=0,done=0;
         nb=(int) count;
         printk(KERN_ALERT"La valeur de nb est : %d (%s:%u)\n",nb, __FUNCTION__, __LINE__);
-        if((flip->f_flags)& O_NONBLOCK){
+        if(down_interruptible(&(Buffer_Tool.SemBuf))!=0)
+            return -ERESTARTSYS;
+/**
+ * Cette boucle while est inspiree par la documentation du kernel linux chapitre 6.2
+ * http://www.makelinux.net/ldd3/chp-6-sect-2
+ */
+        while(Le_buffer.BufEmpty){
+            up(&(Buffer_Tool.SemBuf));
+            if((flip->f_flags)& O_NONBLOCK){
+                return -EAGAIN;
+            }
+            printk(KERN_WARNING"Aucune donnee, Le processus va dormir (%s:%u)\n", __FUNCTION__, __LINE__);
+            if(wait_event_interruptible(reading_queue, ((Le_buffer.BufEmpty) != 1))){
+                return -ERESTARTSYS;
+            }
+            if (down_interruptible(&(Buffer_Tool.SemBuf))){
+                return -ERESTARTSYS;
+            }
+        }
+        printk(KERN_WARNING"Donnee disponible, Le processus est reveille (%s:%u)\n", __FUNCTION__, __LINE__);
 
-            if(down_trylock(&(Buffer_Tool.SemBuf))!=0){
-                printk(KERN_ALERT"Semaphore indisponible  (%s:%u)\n", __FUNCTION__, __LINE__);
-                return -EWOULDBLOCK;
+        do{
+            if((nb-i)<=READWRITE_BUFSIZE){
+                printk(KERN_ALERT"ALMOST DONE\n");
+                done=1;
+                for(k=0;k<(nb-i);k++){
+                    if(BufOut(&Le_buffer,&(Buffer_Tool.ReadBuf[k]))!=0){
+                        for(j=0;j<k;j++){
+                            copy_to_user(&(ubuf[i+j]),&(Buffer_Tool.ReadBuf[j]),1);
+                        }
+                        up(&(Buffer_Tool.SemBuf));
+                        wake_up_interruptible(&(writing_queue));
+                        printk(KERN_ALERT"EXIT SKETCH 1");
+                        return (ssize_t) (i+k);
+                    }
+                }
             }
             else{
-                printk(KERN_ALERT"Semaphore disponible  (%s:%u)\n", __FUNCTION__, __LINE__);
-                i=0;
-                do{
-                    if((nb-i)<=READWRITE_BUFSIZE){
-                        printk(KERN_ALERT"ALMOST DONE\n");
-                        done=1;
-                        for(k=0;k<(nb-i);k++){
-                            if(BufOut(&Le_buffer,&(Buffer_Tool.ReadBuf[k]))!=0){
-                                for(j=0;j<k;j++){
-                                    copy_to_user(&(ubuf[i+j]),&(Buffer_Tool.ReadBuf[j]),1);
-                                }
-                                up(&(Buffer_Tool.SemBuf));
-                                printk(KERN_ALERT"EXIT SKETCH 1");
-                                return (ssize_t) (i+k);
-                            }
+                for(j=0;j<READWRITE_BUFSIZE;j++){
+                    if(BufOut(&Le_buffer,&(Buffer_Tool.ReadBuf[j]))!=0){
+                        for(l=0;l<j;l++){
+                            copy_to_user(&(ubuf[i+l]),&(Buffer_Tool.ReadBuf[l]),1);
                         }
+                        up(&(Buffer_Tool.SemBuf));
+                        printk(KERN_ALERT"EXIT SKETCH 2");
+                        wake_up_interruptible(&(writing_queue));
+                        return (ssize_t) (i+j);
                     }
-                    else{
-                        for(j=0;j<READWRITE_BUFSIZE;j++){
-                            if(BufOut(&Le_buffer,&(Buffer_Tool.ReadBuf[j]))!=0){
-                                for(l=0;l<j;l++){
-                                    copy_to_user(&(ubuf[i+l]),&(Buffer_Tool.ReadBuf[l]),1);
-                                }
-                                up(&(Buffer_Tool.SemBuf));
-                                printk(KERN_ALERT"EXIT SKETCH 2");
-                                return (ssize_t) (i+j);
-                            }
-                        }
+                }
 
-                    }
-                    for(j=0;j<TEST(done,k);j++){
-                        copy_to_user(&(ubuf[i+j]),&(Buffer_Tool.ReadBuf[j]),1);
-                        printk(KERN_ALERT"CHARACTER COPIED TO USER : %d %c %c (%s:%u)\n",(i+j),Buffer_Tool.ReadBuf[j],ubuf[i+j], __FUNCTION__, __LINE__);
-                    }
-                    i+=READWRITE_BUFSIZE;
-                }while(done == 0);
-                up(&(Buffer_Tool.SemBuf));
             }
-        }
-        else{
-            down_interruptible(&(Buffer_Tool.SemBuf));
-            i=0;
-            do{
-                if((nb-i)<=READWRITE_BUFSIZE){
-                    printk(KERN_ALERT"ALMOST DONE\n");
-                    done=1;
-                    for(k=0;k<(nb-i);k++){
-                        if(BufOut(&Le_buffer,&(Buffer_Tool.ReadBuf[k]))!=0){
-                            for(j=0;j<k;j++){
-                                copy_to_user(&(ubuf[i+j]),&(Buffer_Tool.ReadBuf[j]),1);
-                            }
-                            up(&(Buffer_Tool.SemBuf));
-                            printk(KERN_ALERT"EXIT SKETCH 1");
-                            return (ssize_t) (i+k);
-                        }
-                    }
-                }
-                else{
-                    for(j=0;j<READWRITE_BUFSIZE;j++){
-                        if(BufOut(&Le_buffer,&(Buffer_Tool.ReadBuf[j]))!=0){
-                            for(l=0;l<j;l++){
-                                copy_to_user(&(ubuf[i+l]),&(Buffer_Tool.ReadBuf[l]),1);
-                            }
-                            up(&(Buffer_Tool.SemBuf));
-                            printk(KERN_ALERT"EXIT SKETCH 2");
-                            return (ssize_t) (i+j);
-                        }
-                    }
-
-                }
-                for(j=0;j<TEST(done,k);j++){
-                    copy_to_user(&(ubuf[i+j]),&(Buffer_Tool.ReadBuf[j]),1);
-                    printk(KERN_ALERT"CHARACTER COPIED TO USER : %d %c %c (%s:%u)\n",(i+j),Buffer_Tool.ReadBuf[j],ubuf[i+j], __FUNCTION__, __LINE__);
-                }
-                i+=READWRITE_BUFSIZE;
-            }while(done == 0);
+            for(j=0;j<TEST(done,k);j++){
+                copy_to_user(&(ubuf[i+j]),&(Buffer_Tool.ReadBuf[j]),1);
+                printk(KERN_ALERT"CHARACTER COPIED TO USER : %d %c %c (%s:%u)\n",(i+j),Buffer_Tool.ReadBuf[j],ubuf[i+j], __FUNCTION__, __LINE__);
+            }
+            i+=READWRITE_BUFSIZE;
+        }while(done == 0);
 
 
-            up(&(Buffer_Tool.SemBuf));
+        up(&(Buffer_Tool.SemBuf));
 
-        }
+    wake_up_interruptible(&(writing_queue));
     return count;
 }
 static ssize_t Buf_write (struct file *flip, const char __user *ubuf, size_t count,
@@ -357,89 +335,62 @@ static ssize_t Buf_write (struct file *flip, const char __user *ubuf, size_t cou
         int page_write=0,done=0,nb=0,i=0,j=0;
         nb= (int) count;
         printk(KERN_ALERT"La valeur de count est : %zu %d (%s:%u)\n",count, nb, __FUNCTION__, __LINE__);
-        if((flip->f_flags)& O_NONBLOCK){
-            if(down_trylock(&(Buffer_Tool.SemBuf))!=0){
-                printk(KERN_ALERT"Semaphore indisponible  (%s:%u)\n", __FUNCTION__, __LINE__);
-                return -EWOULDBLOCK;
+        if(down_interruptible(&(Buffer_Tool.SemBuf))!=0)
+            return -ERESTARTSYS;
+
+        while(Le_buffer.BufFull){
+            up(&(Buffer_Tool.SemBuf));
+            if((flip->f_flags)& O_NONBLOCK){
+                return -EAGAIN;
+            }
+            printk(KERN_WARNING"Buffer plein, Le processus va dormir (%s:%u)\n", __FUNCTION__, __LINE__);
+            if(wait_event_interruptible(writing_queue, ((Le_buffer.BufFull) != 1))){
+                return -ERESTARTSYS;
+            }
+            if (down_interruptible(&(Buffer_Tool.SemBuf))){
+                return -ERESTARTSYS;
+            }
+        }
+        printk(KERN_WARNING"Place disponible, Le processus est reveille (%s:%u)\n", __FUNCTION__, __LINE__);
+
+        do{
+            if((nb-READWRITE_BUFSIZE)<=0){
+                done=1;
+                printk(KERN_ALERT"Done preparing exit (%s:%u)\n", __FUNCTION__, __LINE__);
+                for(i=0;i<nb;i++){
+                    copy_from_user(&(Buffer_Tool.WriteBuf[i]),&(ubuf[j+i]),1);
+                }
             }
             else{
-                printk(KERN_ALERT"Semaphore disponible  (%s:%u)\n", __FUNCTION__, __LINE__);
-                do{
-                    if((nb-READWRITE_BUFSIZE)<=0){
-                        done=1;
-                        printk(KERN_ALERT"Done preparing exit (%s:%u)\n", __FUNCTION__, __LINE__);
-                        for(i=0;i<nb;i++){
-                            copy_from_user(&(Buffer_Tool.WriteBuf[i]),&(ubuf[j+i]),1);
-                            printk(KERN_ALERT"Charatere ecrit : %d %c %c (%s:%u)\n",i,Buffer_Tool.WriteBuf[i],ubuf[j+i], __FUNCTION__, __LINE__);
-                        }
-                    }
-                    else{
-                        for(i=0;i<READWRITE_BUFSIZE;i++){
-                            copy_from_user(&(Buffer_Tool.WriteBuf[i]),&(ubuf[j+i]),1);
-                            printk(KERN_ALERT"Charatere ecrit : %d %c %c (%s:%u)\n",i,Buffer_Tool.WriteBuf[i],ubuf[i], __FUNCTION__, __LINE__);
-                        }
-                        nb=nb-READWRITE_BUFSIZE;
-                        page_write=page_write+READWRITE_BUFSIZE;
-                        printk(KERN_ALERT"Value of nb %d (%s:%u)\n", nb, __FUNCTION__, __LINE__);
-                    }
-                    i=0;
-                    while( i < TEST(done,nb)){
-                        if(BufIn(&Le_buffer,&(Buffer_Tool.WriteBuf[i]))!=0){
-                            printk(KERN_ALERT"Couldn't push all requested data in buffer (successuful %d) (%s:%u)\n",(i+j), __FUNCTION__, __LINE__);
-                            up(&(Buffer_Tool.SemBuf));
-                            return (ssize_t) (j+i);
-                        }
-                        i++;
-                    }
-                    j+=(READWRITE_BUFSIZE);
-                    printk(KERN_ALERT"Wrote a page in buffer (successuful %d) (%s:%u)\n",i, __FUNCTION__, __LINE__);
-                }while(done == 0 && Le_buffer.BufFull==0);
-                up(&(Buffer_Tool.SemBuf));
-                return 1;
+                for(i=0;i<READWRITE_BUFSIZE;i++){
+                    copy_from_user(&(Buffer_Tool.WriteBuf[i]),&(ubuf[j+i]),1);
+                }
+                nb=nb-READWRITE_BUFSIZE;
+                page_write=page_write+READWRITE_BUFSIZE;
+                printk(KERN_ALERT"Value of nb %d (%s:%u)\n", nb, __FUNCTION__, __LINE__);
             }
-        }
-        else{
-            printk(KERN_ALERT "Mode pouvant etre bloque (%s:%u)\n", __FUNCTION__, __LINE__);
-            down_interruptible(&(Buffer_Tool.SemBuf));
-            printk(KERN_ALERT "Semaphore capturer (%s:%u)\n", __FUNCTION__, __LINE__);
+            i=0;
+            while( i < TEST(done,nb)){
+               if(BufIn(&Le_buffer,&(Buffer_Tool.WriteBuf[i]))!=0){
+                    printk(KERN_ALERT"Couldn't push all requested data in buffer (successuful %d) (%s:%u)\n",(j+i), __FUNCTION__, __LINE__);
+                    up(&(Buffer_Tool.SemBuf));
+                    wake_up_interruptible(&(reading_queue));
+                    return (ssize_t) (j+i);
+               }
+               printk(KERN_ALERT"Charatere ecrit : %d %c %c (%s:%u)\n",i,Buffer_Tool.WriteBuf[i],Le_buffer.Buffer[i], __FUNCTION__, __LINE__);
+               i++;
 
-            do{
-                if((nb-READWRITE_BUFSIZE)<=0){
-                    done=1;
-                    printk(KERN_ALERT"Done preparing exit (%s:%u)\n", __FUNCTION__, __LINE__);
-                    for(i=0;i<nb;i++){
-                        copy_from_user(&(Buffer_Tool.WriteBuf[i]),&(ubuf[j+i]),1);
-                    }
-                }
-                else{
-                    for(i=0;i<READWRITE_BUFSIZE;i++){
-                        copy_from_user(&(Buffer_Tool.WriteBuf[i]),&(ubuf[j+i]),1);
-                    }
-                    nb=nb-READWRITE_BUFSIZE;
-                    page_write=page_write+READWRITE_BUFSIZE;
-                    printk(KERN_ALERT"Value of nb %d (%s:%u)\n", nb, __FUNCTION__, __LINE__);
-                }
-                i=0;
-                while( i < TEST(done,nb)){
-                   if(BufIn(&Le_buffer,&(Buffer_Tool.WriteBuf[i]))!=0){
-                        printk(KERN_ALERT"Couldn't push all requested data in buffer (successuful %d) (%s:%u)\n",(j+i), __FUNCTION__, __LINE__);
-                        up(&(Buffer_Tool.SemBuf));
-                        return (ssize_t) (j+i);
-                   }
-
-                    printk(KERN_ALERT"Charatere ecrit : %d %c %c (%s:%u)\n",i,Buffer_Tool.WriteBuf[i],Le_buffer.Buffer[i], __FUNCTION__, __LINE__);
-                    i++;
-
-                }
-                j+=(READWRITE_BUFSIZE);
-                printk(KERN_ALERT"Wrote a page in buffer (successuful %d) (%s:%u)\n",i, __FUNCTION__, __LINE__);
-            }while(done == 0 && Le_buffer.BufFull==0);
-            up(&(Buffer_Tool.SemBuf));
-            return 1;
-        }
-    printk(KERN_ALERT"Fonction finie, sortons de la fonctions (%s:%u)\n", __FUNCTION__, __LINE__);
-    return 1;
+            }
+            j+=(READWRITE_BUFSIZE);
+            printk(KERN_ALERT"Wrote a page in buffer (successuful %d) (%s:%u)\n",i, __FUNCTION__, __LINE__);
+        }while(done == 0 && Le_buffer.BufFull==0);
+        up(&(Buffer_Tool.SemBuf));
+        printk(KERN_ALERT"Fonction finie, sortons de la fonctions (%s:%u)\n", __FUNCTION__, __LINE__);
+        wake_up_interruptible(&(reading_queue));
+        return count;
 }
+
+
 long Buf_ioctl (struct file *flip, unsigned int cmd, unsigned long arg){
     int buffer=0;
     switch (cmd){
